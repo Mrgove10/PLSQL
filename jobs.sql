7d95982d0831ae82a3a71754a2c191e66f4103d3
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-22 20:39:07 +0100

    jobs

--- jobs.sql
+++ jobs.sql
@@ -1,3 +1,19 @@
 BEGIN 
-    package1.clearjobs(SYSDATE);
+--clean jobs
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution)VALUES('test1','0','chemin/du/test/1.sql','31/12/20');
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution)VALUES('test2','0','chemin/du/test/2.sql','11/11/19');
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution)VALUES('test3','0','chemin/du/test/3.sql','09/09/19');
+    package1.cleanjobs(SYSDATE); --fonctionne
+END;
+
+--executeJobs
+declare
+    type NumberVarray is varray(100) of NUMERIC(10);
+    myArray NumberVarray;
+BEGIN
+    myArray := NumberVarray(1,2);
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution) VALUES('test1','0','chemin/du/test/1.sql','31/12/20');
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution) VALUES('test2','0','chemin/du/test/2.sql','11/11/19');
+    INSERT INTO TJOB(nom,typejob,chemin,datederniereexecution) VALUES('test3','0','chemin/du/test/3.sql','09/09/19');
+    package1.executeJobs(myArray); --ne fonctionne pas 
 END;
